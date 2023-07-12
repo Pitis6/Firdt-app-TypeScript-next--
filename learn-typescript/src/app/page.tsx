1e7a5aeede02cc06ff3d0cfde3e1adaad0df147a
pitis6 <felipecalvache6@gmail.com>
2023-07-11 22:06:04 -0500

    Start learn typescript

---
+++ learn-typescript/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="text-3xl font-bold underline">
+      <h1>Hello Platzi!</h1>
+    </main>
+  );
+}
