92b223ed6b600ca7167b79b75b7758e34d60d7b9
pitis6 <felipecalvache6@gmail.com>
2023-07-11 22:54:42 -0500

    Add State

--- learn-typescript/src/app/components/RandomFox.tsx
+++ learn-typescript/src/app/components/RandomFox.tsx
@@ -1,5 +1,24 @@
-export const RandomFox = () => {
-    return(
-        <img/>
-    )
-}
+import { type } from "os";
+import type { FunctionComponent, FC } from "react";
+
+
+
+// export const RandomFox = () => {
+//     return(
+//         <img/>
+//     )
+// }
+
+type Props = {image: string}
+
+export const RandomFox = ({image}: Props): JSX.Element => {
+  return <img width={320} height='auto' src={image} className="rounded" />;
+};
+
+// export const RandomFox: FunctionComponent = () => {
+//   return <img />;
+// };
+
+// export const RandomFox: FC = () => {
+//   return <img />;
+// };
--- learn-typescript/src/app/page.tsx
+++ learn-typescript/src/app/page.tsx
@@ -1,9 +1,29 @@
-import Image from 'next/image'
+'use client'
+// import Image from 'next/image'
+import { RandomFox } from './components/RandomFox';
+import { useState } from 'react';
+
+const ramdom = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+const [images, setimages] = useState<string[]>([
+  `http://randomfox.ca/images/${ramdom()}.jpg`,
+  `http://randomfox.ca/images/${ramdom()}.jpg`,
+  `http://randomfox.ca/images/${ramdom()}.jpg`,
+  `http://randomfox.ca/images/${ramdom()}.jpg`,
+]);
+
+
   return (
     <main className="text-3xl font-bold underline">
       <h1>Hello Platzi!</h1>
+      {
+        images.map((image, index)=>(
+          <div key={index} className='p-4'>
+            <RandomFox image={image}/>
+          </div>
+        ))
+      }
     </main>
   );
 }
